7b03868353623a17e61ee39875e6d48ad8d32b5e
Regan Anderson <llisandur@outlook.com>
2025-06-25 18:38:39 -0500

    Update manifest Switch to example scripts

--- src/scripts/init.lua
+++ src/scripts/init.lua
@@ -1,31 +1,44 @@
-
+-- entry point for all lua code of the pack
+-- more info on the lua API: https://github.com/black-sliver/PopTracker/blob/master/doc/PACKS.md#lua-interface
+ENABLE_DEBUG_LOG = true
+-- get current variant
 local variant = Tracker.ActiveVariantUID
+-- check variant info
+IS_ITEMS_ONLY = variant:find("itemsonly")
 
--- Items
-require("scripts/items_import")
+print("-- Example Tracker --")
+print("Loaded variant: ", variant)
+if ENABLE_DEBUG_LOG then
+    print("Debug logging is enabled!")
+end
+
+-- Utility Script for helper functions etc.
+ScriptHost:LoadScript("scripts/utils.lua")
 
 -- Logic
-require("scripts/logic/logic_helper")
-require("scripts/logic/logic_main")
+ScriptHost:LoadScript("scripts/logic/logic.lua")
 
--- Maps
-if Tracker.ActiveVariantUID == "maps-u" then
-    Tracker:AddMaps("maps/maps-u.json")  
-else
-    Tracker:AddMaps("maps/maps.json")  
-end  
+-- Custom Items
+ScriptHost:LoadScript("scripts/custom_items/class.lua")
+ScriptHost:LoadScript("scripts/custom_items/progressiveTogglePlus.lua")
+ScriptHost:LoadScript("scripts/custom_items/progressiveTogglePlusWrapper.lua")
 
-if PopVersion and PopVersion >= "0.23.0" then
-    Tracker:AddLocations("locations/dungeons.json")
+-- Items
+Tracker:AddItems("items/items.jsonc")
+
+if not IS_ITEMS_ONLY then -- <--- use variant info to optimize loading
+    -- Maps
+    Tracker:AddMaps("maps/maps.jsonc")
+    -- Locations
+    Tracker:AddLocations("locations/locations.jsonc")
 end
 
 -- Layout
-require("scripts/layouts_import")
-
--- Locations
-require("scripts/locations_import")
+Tracker:AddLayouts("layouts/items.jsonc")
+Tracker:AddLayouts("layouts/tracker.jsonc")
+Tracker:AddLayouts("layouts/broadcast.jsonc")
 
 -- AutoTracking for Poptracker
 if PopVersion and PopVersion >= "0.26.0" then
-    require("scripts/autotracking")
+    ScriptHost:LoadScript("scripts/autotracking.lua")
 end
