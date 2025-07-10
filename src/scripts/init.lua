529091ac9ede49e8c710d95e0269a6f8f4616aea
Regan Anderson <llisandur@outlook.com>
2025-07-10 13:49:04 -0500

    Update earth cave

--- src/scripts/init.lua
+++ src/scripts/init.lua
@@ -43,6 +43,7 @@ if not IS_ITEMS_ONLY then -- <--- use variant info to optimize loading
   Tracker:AddLocations("locations/dungeons/cardia_islands.jsonc")
   Tracker:AddLocations("locations/dungeons/temple_of_fiends.jsonc")
   Tracker:AddLocations("locations/dungeons/marsh_cave.jsonc")
+  Tracker:AddLocations("locations/dungeons/earth_cave.jsonc")
 end
 
 -- Layout
