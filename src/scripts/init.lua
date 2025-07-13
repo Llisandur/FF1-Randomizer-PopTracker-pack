6e2fae53ef77759564ba70ff2fbfeead7f700efb
Regan Anderson <llisandur@outlook.com>
2025-07-12 23:01:05 -0500

    Update temple of fiends revisited

--- src/scripts/init.lua
+++ src/scripts/init.lua
@@ -51,6 +51,7 @@ if not IS_ITEMS_ONLY then -- <--- use variant info to optimize loading
   Tracker:AddLocations("locations/dungeons/waterfall_cave.jsonc")
   Tracker:AddLocations("locations/dungeons/mirage_tower.jsonc")
   Tracker:AddLocations("locations/dungeons/sky_fortress.jsonc")
+  Tracker:AddLocations("locations/dungeons/temple_of_fiends_revisited.jsonc")
 end
 
 -- Layout
